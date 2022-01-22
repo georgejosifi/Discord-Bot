992ee850f5fdc44b4218524eeb1df8eae10df451
Gjergji Josifi <gjergji.josifi@gmail.com>
2022-01-22 23:25:35 +0100

    Empty commit

--- src/main/java/events/ElvisGreetingAdapter.java
+++ src/main/java/events/ElvisGreetingAdapter.java
@@ -9,11 +9,11 @@ public class ElvisGreetingAdapter extends ListenerAdapter {
     @Override
     public void onUserUpdateOnlineStatus(@NotNull UserUpdateOnlineStatusEvent event) {
         String elvisId = System.getenv("ElvisId");
-        String xhoiId = System.getenv("XhoiId");
+        String njoiId = System.getenv("XhoiId");
         if(event.getMember().getId().equals(elvisId) &&
                 event.getNewOnlineStatus().getKey().equalsIgnoreCase("online")) {
             event.getGuild().getDefaultChannel().sendMessage("Mireseerdhe Elvis prototipi 2.0").queue();
-        }else if(event.getMember().getId().equals(xhoiId) &&
+        }else if(event.getMember().getId().equals(njoiId) &&
                 event.getNewOnlineStatus().getKey().equalsIgnoreCase("online")) {
             event.getGuild().getDefaultChannel().sendMessage("Uuuuu erdhi Admini!").queue();
         }
